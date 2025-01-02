2a3d57f785d40508bb5be5e3ec190714a04bf558
Bruno Mazzo <mazzo.bruno@gmail.com>
2025-01-02 13:59:45 +1000

    Try to optimize godata logic

--- lua/neotest-golang/lib/json.lua
+++ lua/neotest-golang/lib/json.lua
@@ -32,6 +32,36 @@ function M.decode_from_table(tbl, construct_invalid)
   return jsonlines
 end
 
+
+--- Decode JSON from a table of strings into a table of objects.
+--- @param tbl table
+--- @param construct_invalid boolean
+--- @return table
+function M.decode_godata_from_table(tbl, construct_invalid)
+  local jsonlines = {}
+  for _, line in ipairs(tbl) do
+    nio.sleep(0)
+    if string.match(line, "^%s*{") then -- must start with the `{` character
+      local status, json_data = pcall(vim.json.decode, line)
+      if status then
+        if json_data.TestGoFiles ~= nil or json_data.XTestGoFiles ~= nil then
+        table.insert(jsonlines, json_data)
+        end
+      else
+        -- NOTE: this can be hit because of "Vim:E474: Unidentified byte: ..."
+        logger.warn("Failed to decode JSON line: " .. line)
+      end
+    else
+      logger.debug({ "Not valid JSON:", line })
+      if construct_invalid then
+        -- this is for example errors from stderr, when there is a compilation error
+        table.insert(jsonlines, { Action = "output", Output = line })
+      end
+    end
+  end
+  return jsonlines
+end
+
 --- Decode JSON from a string into a table of objects.
 --- @param str string
 --- @return table
@@ -50,5 +80,25 @@ function M.decode_from_string(str)
   table.insert(tbl, current_object)
   return M.decode_from_table(tbl, false)
 end
+
+--- Decode JSON from a string into a table of objects.
+--- @param str string
+--- @return table
+function M.decode_godata_from_string(str)
+  -- Split the input into separate JSON objects
+  local tbl = {}
+  local current_object = ""
+  for line in str:gmatch("[^\r\n]+") do
+    nio.sleep(0)
+    if line:match("^%s*{") and current_object ~= "" then
+      table.insert(tbl, current_object)
+      current_object = ""
+    end
+    current_object = current_object .. line
+  end
+  table.insert(tbl, current_object)
+  return M.decode_godata_from_table(tbl, false)
+end
+
 
 return M
